8436dfa42430954a790f3838128107db50d2af65
sandriek <sjlkemme@avans.nl>
2016-03-14 22:56:02 +0100

    week 3 nu compleet t/m 3

---
+++ week 3/week3.3.c
@@ -0,0 +1,56 @@
+/*
+ * Week3.3.c
+ *
+ * Created: 3/8/2016 11:13:00 AM
+ * Author : Tom Remeeus
+ */ 
+
+#define Bit(x)(1<<x)
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int msCount = 0;
+int LEDswitch = 0;
+
+void ToggleD7(void)
+{
+	PORTD ^= Bit(7);
+}
+
+void init(void)
+{
+	DDRD |= Bit(7);
+
+	OCR2 = 250;
+	TIMSK |= Bit(7);
+	sei();
+	TCCR2 = 0b00001101;
+}
+
+ISR(TIMER2_COMP_vect)
+{
+	msCount ++;
+
+	if(LEDswitch && msCount == 15)
+	{
+		LEDswitch = 0;
+		msCount = 0;
+		ToggleD7();
+	}
+	else if(!LEDswitch && msCount == 25)
+	{
+		LEDswitch = 1;
+		msCount = 0;
+		ToggleD7();
+	}
+}
+
+int main(void)
+{
+	init();
+	
+	while (1)
+	{
+	}
+}
